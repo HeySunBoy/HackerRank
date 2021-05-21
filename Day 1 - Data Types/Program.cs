6aa36cb9f2d5f7a0e5fc0b5c42062e0d744c3836
GaryChen <tp841025@gmail.com>
2021-05-21 19:15:22 +0800

    1. 上傳三天的紀錄

---
+++ Day 1 - Data Types/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Data_Types
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+
+            // Declare second integer, double, and String variables.
+            int read_i;
+            double read_d;
+            string read_s;
+            // Read and save an integer, double, and String to your variables.
+            read_i = int.Parse(Console.ReadLine());
+            read_d = double.Parse(Console.ReadLine());
+            read_s = Console.ReadLine();
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine(i + read_i);
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine(string.Format("{0:N1}",d + read_d));
+            // Concatenate and print the String variables on a new line
+            Console.WriteLine(s + read_s);
+            // The 's' variable above should be printed first.
+
+        }
+    }
+}
---
+++ Day 3 - Intro to Conditional Statements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Day_3___Intro_to_Conditional_Statements
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+            if (N % 2 != 0)
+            {
+                Console.Write("Weird");
+            }
+            else if (N <= 5)
+            {
+                Console.Write("Not Weird");
+            }
+            else if (N <= 20)
+            {
+                Console.Write("Weird");
+            }
+            else
+            {
+                Console.Write("Not Weird");
+            }
+        }
+    }
+
+}
