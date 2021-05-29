06d42550ce833197f6a3db61c8f4e648c25b1f1f
GaryChen <tp841025@gmail.com>
2021-05-29 21:15:24 +0800

    1. Day10 & Day11 2. upload .sln

---
+++ Day 11 - 2D Arrays/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int max = int.MinValue;
+
+        for (int j = 0; j < 4; j++)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                int sum = 0;
+                sum += arr[j][i] + arr[j][i + 1] + arr[j][i + 2];
+                sum += arr[j + 1][i + 1];
+                sum += arr[j + 2][i] + arr[j + 2][i + 1] + arr[j + 2][i + 2];
+
+                if (sum > max)
+                {
+                    max = sum;
+                }
+            }
+        }
+
+        Console.WriteLine(max);
+    }
+}
