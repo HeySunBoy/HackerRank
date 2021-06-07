642a8a53baaf09e9022089ed034cf42205133fe2
GaryChen <tp841025@gmail.com>
2021-06-07 21:12:27 +0800

    1. Day 20

---
+++ Day 20 - Sorting/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        // Write your code here
+        int swap_count = 0;
+
+        for (int i = a.Count - 1; i > 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (a[j] > a[j + 1])
+                {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    swap_count++;
+                }
+            }
+        }
+
+        Console.WriteLine($"Array is sorted in {swap_count} swaps.");
+        Console.WriteLine($"First Element: {a.First()}");
+        Console.WriteLine($"Last Element: {a.Last()}");
+
+    }
+}
